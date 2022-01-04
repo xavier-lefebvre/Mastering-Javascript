316d61f9d128ec36f1acb82d167d12db3b5a808f
xavier-lefebvre <lefebvrexavier60@gmail.com>
2022-01-04 10:29:26 +0100

    Objects --> Level02 ongoing

---
+++ day_02/Objects_Level02.js
@@ -0,0 +1,62 @@
+const users = {
+    Alex: {
+      email: 'alex@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript'],
+      age: 20,
+      isLoggedIn: false,
+      points: 30
+    },
+    Asab: {
+      email: 'asab@asab.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 25,
+      isLoggedIn: false,
+      points: 50
+    },
+    Brook: {
+      email: 'daniel@daniel.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+      age: 30,
+      isLoggedIn: true,
+      points: 50
+    },
+    Daniel: {
+      email: 'daniel@alex.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    John: {
+      email: 'john@john.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+      age: 20,
+      isLoggedIn: true,
+      points: 50
+    },
+    Thomas: {
+      email: 'thomas@thomas.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    },
+    Paul: {
+      email: 'paul@paul.com',
+      skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+      age: 20,
+      isLoggedIn: false,
+      points: 40
+    }
+  };
+// 1. Trouvez la personne qui possède de nombreuses compétences dans l'objet users.
+// C'est Asab
+
+// 2. Comptez les users connectés, comptez les users ayant plus de 50 points de l'objet suivant.
+// Il y a un users connecté et 3 users ayant 50 points.
+
+// 3. Recherchez des personnes qui sont des développeurs MERN Stack à partir de l'objet users. MERN (MongoDB, Express, React, Node)
+
+ 
+
+console.log(Object.values(users.Paul.skills));
